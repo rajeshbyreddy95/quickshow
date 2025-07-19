ca0979194af4afd008280406ae0b5a1415313d8f
Sreejib-Nandy <sreejibnandy2518@gmail.com>
2025-07-19 21:38:46 +0530

    fixing

--- server/Control/Stripewebhooks.js
+++ server/Control/Stripewebhooks.js
@@ -9,35 +9,42 @@ export const stripeWebhooks = async (request, response) => {
 
     let event;
     try {
-        event = stripeInstance.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_KEY)
+        event = stripeInstance.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_KEY);
+        console.log(event);
     } catch (error) {
         return response.status(400).send(`Webhook error : ${error.message}`);
     }
 
     try {
         switch (event.type) {
-            case "payment_intent.succeeded": {
+            case "checkout.session.completed": {
                 const paymentIntent = event.data.object;
+                console.log(paymentIntent);
                 const sessionList = await stripeInstance.checkout.sessions.list({
                     payment_intent: paymentIntent.id,
                 })
                 console.log("✅ PaymentIntent succeeded:", paymentIntent.id);
-                console.log("📦 Looking up session for payment_intent:", paymentIntent.id);
                 console.log("🧾 Sessions found:", sessionList.data.length);
 
                 const session = sessionList.data[0];
+                console.log(session);
                 const { bookingId } = session.metadata;
                 console.log("🆔 Booking ID:", bookingId);
                 console.log("🧪 Type of bookingId:", typeof bookingId);
                 const { ObjectId } = mongoose.Types;
+                console.log(ObjectId);
                 if (!ObjectId.isValid(bookingId)) {
                     console.log("❌ Invalid booking ID:", bookingId);
                     return response.status(400).send("Invalid booking ID");
                 }
-                const updatedBooking = await Booking.findByIdAndUpdate(new ObjectId(bookingId), {
+                const booking = await Booking.findById(bookingId);
+                console.log("🔍 Booking found in DB:", booking);
+                const updatedBooking = await Booking.findByIdAndUpdate(bookingId, {
                     isPaid: true,
                     paymentLink: '',
-                }, { new: true })
+                })
+                const booking2 = await Booking.findById(bookingId);
+                console.log(booking2);
                 console.log("📘 Updated Booking:", updatedBooking);
                 break;
             }
